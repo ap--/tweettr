89bf595b038175210a738964c2d3510d5659bdc9
ap-- <andreas@poehlmann.io>
2020-02-16 02:28:01 +0100

    [tweettr] initial commit

---
+++ tweettr/__init__.py
@@ -0,0 +1,5 @@
+
+from ._objects import (BoundingBox, Coordinates, Emoji, Entities,
+                       ExtendedEntities, ExtendedTweet, HashTag, Media,
+                       Place, Poll, QuotedStatusPermalink, Size, Sizes,
+                       Symbol, Tweet, Url, User, UserMention)
